7d9166e89f747380bc97b652736746fd0c2d62e6
Kyle Lejeune <kylejl98@gmail.com>
2023-05-06 23:11:40 -0400

    passed test 1

--- exercises/e1.js
+++ exercises/e1.js
@@ -5,7 +5,8 @@ import { data } from "../data/data";
 // Return example: ['name1', 'name2', ... , 'nameN']
 
 export function getPlanetNames(data) {
-  return data.map((planet) => planet.name);
+  let planetNames = data.planets.map((planet) => planet.name)
+  return planetNames;
 }
 
 
