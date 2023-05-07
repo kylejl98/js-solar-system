81b6433357ab1fd4d410a650c1972ef4c9adedcf
Kyle Lejeune <kylejl98@gmail.com>
2023-05-07 13:00:43 -0400

    passed test 10

--- exercises/e9.js
+++ exercises/e9.js
@@ -8,8 +8,6 @@ export function getEarthData(data) {
   return data.planets.find((planet) => planet.name === "Earth")
 }
 
-
-
 // === TEST YOURSELF ===
 // Once you're finished run the test with "npm run test-9"
 // If the test has all tests passed, switch to the next exercise file
